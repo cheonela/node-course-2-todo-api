05f7964562838d59e0cbd61cd2409a36b65c3a41
Cheong <Elaine.Cheong@adidas-group.com>
2018-05-02 14:44:13 -0400

    Add get todos

--- server/server.js
+++ server/server.js
@@ -35,6 +35,15 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+
 
 // It is used to bind the applicatiion to the port
 app.listen(3000, () => {
