8f034778034b9dc3e97cc6f47fdd0c56d2119868
Cheong <Elaine.Cheong@adidas-group.com>
2018-05-08 09:51:02 -0400

    lesson 80-2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,11 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Check if having the server for mongodb, if not, then use local host
-mongoose.connect(process.env.MONOGODB_URI || 'mongodb://localhost:27017/NewTodoApp');
+let db = {
+  localhost: 'mongodb://localhost:27017/NewTodoApp',
+  mlab: 'mongodb://dbtest:lesson80@ds217360.mlab.com:17360/todoapp'
+}
+mongoose.connect(db.mlab || db.localhost);
 
 // We need to export the mongoose
 module.exports = {mongoose}
