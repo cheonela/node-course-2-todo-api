7fa4c4890466425f0e5b0803d0db7e2ce4be47d3
Cheong <Elaine.Cheong@adidas-group.com>
2018-05-08 09:32:19 -0400

    setup app for heroku2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // to tell mongoose to use promise rather than callback
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/NewTodoApp');
+mongoose.connect(process.env.MONOGODB_URI || 'mongodb://localhost:27017/NewTodoApp');
 
 // We need to export the mongoose
 module.exports = {mongoose}
